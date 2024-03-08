3f02bd0431a7a8d98992d0896eda4e4c57f18948
gubei6346 <gubeiiii6346@gmail.com>
2024-03-08 14:23:09 +0800

    test

---
+++ vue-base/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h3>模板语法</h3>
+  <p>{{ msg }}</p>
+  <p>{{ hello }}</p>
+  <p>{{ number + 1 }}</p>
+  <p>{{ ok ? 'Yes' : 'No' }}</p>
+  <p>{{ message.split("").reverse().join("") }}</p>
+  <p v-html="rawHtml"></p>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: "vue !!!!",
+      hello: "hello world",
+      number: 10,
+      ok: true,
+      message: "大家好",
+      rawHtml: "<a href='https://bilibili.com'>bilibili</a>"
+    }
+  }
+}
+</script>
+
+
+
+
