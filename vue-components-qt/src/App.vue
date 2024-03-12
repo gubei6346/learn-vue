c3818c50e0e2d836dc8e148ebe1fa904efe3cdd6
gubei6346 <gubeiiii6346@gmail.com>
2024-03-12 12:48:30 +0800

    test

---
+++ vue-components-qt/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Header />
+  <Main />
+  <Aside />
+</template>
+<script>
+import Header from "./pages/Header.vue"
+import Main from "./pages/Main.vue"
+import Aside from "./pages/Aside.vue"
+export default {
+  components: {
+    Header,
+    Main,
+    Aside
+  }
+}
+</script>
+<style></style>
---
+++ vue-components-qt/src/pages/Aside.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="aside">
+        <h3>Aside</h3>
+        <Item />
+        <Item />
+        <Item />
+    </div>
+</template>
+<script>
+import Item from "./Item.vue"
+export default {
+    components: {
+        Item
+    }
+}
+</script>
+<style scoped>
+.aside {
+    float: right;
+    width: 30%;
+    height: 600px;
+    border: 5px solid #999;
+    box-sizing: border-box;
+    border-left: 0;
+    border-top: 0;
+}
+</style>
