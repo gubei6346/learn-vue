d230d10806488e0eee43181c9caf996133f59b17
gubei6346 <gubeiiii6346@gmail.com>
2024-03-13 11:56:43 +0800

    test

--- vue-component-event/src/App.vue
+++ vue-component-event/src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <ComponentEvent />
+  <Main />
 </template>
 <script>
 import ComponentEvent from './components/ComponentEvent.vue';
+import Main from './components/Main.vue';
 export default {
   components: {
-    ComponentEvent
+    ComponentEvent,
+    Main
   }
 }
 </script>
---
+++ vue-component-event/src/components/SearchComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    搜索<input type="text" v-model="search">
+</template>
+<script>
+export default {
+    data() {
+        return {
+            search: ""
+        }
+    },
+    watch: {
+        search(newValue, oldValue) {
+            this.$emit("searchEvent", newValue)
+        }
+    }
+}
+</script>
