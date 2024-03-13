cb2c0d255b180e2753f5707453b985bec87baa58
gubei6346 <gubeiiii6346@gmail.com>
2024-03-13 11:11:27 +0800

    test

---
+++ vue-component-event/src/components/Child.vue
@@ -0,0 +1,19 @@
+<template>
+    <h3>Child</h3>
+    <button @click="clickEventHandle">传递数据</button>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            msg: "Childd数据！！！"
+        }
+    },
+    methods: {
+        clickEventHandle() {
+            //自定义事件
+            this.$emit("someEvent", this.msg)
+        }
+    }
+}
+</script>
---
+++ vue-component-event/src/components/ComponentEvent.vue
@@ -0,0 +1,25 @@
+<template>
+    <h3>组件事件</h3>
+    <Child @someEvent="getHandle" />
+    <p>父元素:{{ message }}</p>
+</template>
+<script>
+import Child from './Child.vue';
+export default {
+    data() {
+        return {
+            message: "更改前"
+        }
+    },
+    components: {
+        Child
+    },
+    methods: {
+        getHandle(data) {
+            console.log("触发了", data)
+            this.message = data;
+
+        }
+    }
+}
+</script>
