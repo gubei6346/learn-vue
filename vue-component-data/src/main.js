d0519829b42f25dae95a87a6060610489a1f2af5
gubei6346 <gubeiiii6346@gmail.com>
2024-03-16 12:17:59 +0800

    test

--- vue-component-data/src/main.js
+++ vue-component-data/src/main.js
@@ -3,6 +3,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+//app:Vue的实例对象
+//在一个vue项目中，有且只有一个vue的实例对象
 const app = createApp(App)
+//App:根组件
 app.provide("globalData", "全局数据")
+//挂载应用 mount  #app---index.html
 app.mount('#app')
